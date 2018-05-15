bb2546774516abc56cb3e7b5a263a24e997f7286
Min <minryu0727@gmail.com>
2018-05-14 21:50:43 -0700

    Added function to update state in app

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = props => (
   <header className="top">
     <h1>
-      Catch of the
+      Catch
       <span className="ofThe">
         <span className="of">of</span>
         <span className="the">The</span>
